466e379c81f6e835a68f6c16636d145b3c7cdf9e
johanthestrup <johanthestrup@gmail.com>
2021-06-06 20:52:19 +0900

    added try/catch to all queries and mutations

--- models/index.ts
+++ models/index.ts
@@ -5,28 +5,13 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-// const config = require(__dirname + "/../config/config.js")[env];
 import { config } from "../config/config";
 const db: any = {};
 
-// let sequelize: any = new Sequelize(
-//   process.env.DATABASE_URL || { dialect: "postgres" }
-// );
 let sequelize: any = new Sequelize(
   process.env.DATABASE_URL ||
     `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`
 );
-
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
-// }
 
 fs.readdirSync(__dirname)
   .filter((file: string) => {
@@ -50,8 +35,5 @@ Object.keys(db).forEach((modelName) => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-//Assosiactions
-// db.Recipe.belongsTo(db.User);
 
 export default db;
