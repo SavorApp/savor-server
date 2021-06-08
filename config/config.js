a2f70a0238ac807d1f996e97d4ce42d084e5e879
johanthestrup <johanthestrup@gmail.com>
2021-06-08 16:34:52 +0900

    changed config file

--- config/config.js
+++ config/config.js
@@ -11,6 +11,11 @@ module.exports = {
   },
   production: {
     dialect: "postgres",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL || {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      host: "127.0.0.1",
+    },
   },
 };
